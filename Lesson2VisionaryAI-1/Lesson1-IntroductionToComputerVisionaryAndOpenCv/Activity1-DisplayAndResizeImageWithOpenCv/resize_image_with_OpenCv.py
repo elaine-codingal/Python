55f471ae0ad863a2c8b73e1d55002f53544fc160
elaine <elainemuhombe.codingal@gmail.com>
2025-03-10 19:48:36 +0300

    Initialize project and add project files

---
+++ Lesson2VisionaryAI-1/Lesson1-IntroductionToComputerVisionaryAndOpenCv/Activity1-DisplayAndResizeImageWithOpenCv/resize_image_with_OpenCv.py
@@ -0,0 +1,16 @@
+import cv2
+
+#Load the image
+image=cv2.imread('dog.jpg')
+
+#Resize the window to a specified size without resizing the image
+cv2.namedWindow('LoadedImage', cv2.WINDOW_NORMAL) #Create a resizable window
+cv2.resizeWindow('LoadedImage',800,500)#Set the window size to 800x500(widthxheight)
+
+#Display the image in the resized window
+cv2.imshow('LoadedImage', image)
+cv2.waitKey(0)#Wait for a key press
+cv2.destroyAllWindows() #Close the window
+
+#Print image properties
+print(f"Image Dimensions:{image.shape}")#Height, Width, Channels
